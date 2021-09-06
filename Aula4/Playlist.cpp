5731e53490c6bce0630426ec6fa78f3fa5144904
viniciusdm <viniciusbuenodemoraes@gmail.com>
2021-09-06 16:52:30 -0300

    djdj

--- Aula4/Playlist.cpp
+++ Aula4/Playlist.cpp
@@ -1,16 +1,5 @@
 #include "Playlist.h"
 
-
-int Playlist::getDuracaoTotal(){
-    for (i = 0, i < 10, i ++)
-
-
-}
-
-bool Playlist::adicionar(Musica* m){
-
-}
-
 void Playlist::setNome(std::string nome){
     this -> nome = nome;
 }
@@ -20,10 +9,55 @@ std::string Playlist::getNome(){
 
 }
 
+int Playlist::getDuracaoTotal(){
+    for (i = 0, i < 10, i ++)
+
+
+}
+
+bool Playlist::adicionar(Musica* m){
+    for (i = 0, i < x, i ++)
+        if (musicas[i] = m || x == NUMERO_MAXIMO_VALORES);
+            return false;        
+    musicas[x] = m;
+    x++;
+    return true;
+}
+
 int Playlist::getQuantidade(){
+    //quantidade = 
+    return = quantidade;
 
 }
 
 void Playlist::imprimir(){
+    cout << nome << " - " << duracao << " segundos no total" << endl;
 
+}
+
+void teste() {
+    Musica *Roses = new Musica;
+    Roses -> setNome("Roses");
+    Roses -> setDuracao(180);
+    Roses -> avaliar(3);
+    Roses -> avaliar(3);
+    Roses -> avaliar(1);
+
+    Musica *Overdue = new Musica;
+    Roses -> setNome("Overdue");
+    Roses -> setDuracao(210);
+    Roses -> avaliar(1);
+    Roses -> avaliar(5);
+    Roses -> avaliar(4);
+    
+    Playslist *Estrangeiras = new Playlist;
+    Estrangeiras -> adicionar(Roses);
+    Estrangeiras -> adicionar(Overdue);
+    Estrangeiras -> imprimir();
+
+}
+
+int main() {
+    teste();
+    return 0;
 }
