cdd56018a65e88026d4feb0ca0717dd5e39da770
viniciusdm <viniciusbuenodemoraes@gmail.com>
2021-09-06 14:56:42 -0300

    oi

--- Aula4/Musica.h
+++ Aula4/Musica.h
@@ -4,15 +4,19 @@
 #include <string>
 
 class Musica {
-public:
+private:
     std::string nome;
     int duracao;
     int quantidadeDeAvalicoes = 0;
     int somaDasAvaliacoes = 0;
 
+public:
+    std::string getNome();
+    void setNome(std::string nome);
+    int getDuracao();
+    void setDuracao(int duracao);
     void avaliar(int nota);
     double getMedia();
-
     void imprimir();
 };
 
