a4afdf0cc2b7c8f6fd735218169ce411d2b3abaf
Vinicius Moraes <viniciusdm@github.com>
2021-09-09 15:44:00 -0300

    oii

--- Testes/bode.py
+++ Testes/bode.py
@@ -1,1 +1,12 @@
-pyth
+msg = "Hello World"
+print(msg)
+
+import numpy as np
+
+a = np.ones(5)
+print(a)
+import pandas as pd
+a = input("escreve ai: ")
+print(a)
+a = input("escreve ai: ")
+print(a)
