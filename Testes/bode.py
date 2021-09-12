fb6ec7b7722796675433b6005d67efe594227b54
viniciusdm <viniciusbuenodemoraes@gmail.com>
2021-09-12 16:17:17 -0300

    tudo

--- Testes/bode.py
+++ Testes/bode.py
@@ -10,12 +10,7 @@ import sklearn
 import scipy 
 import pandas as pd
 import yfinance as pd
-<<<<<<< HEAD:Testes/bode.py
+
 import py
-=======
 import seaborn
-
 import sklearn
-
-e nois 
->>>>>>> 4557336e0e803be4522e82a63e623c2f2dd5dc1d:Testes/python3 bode.py
