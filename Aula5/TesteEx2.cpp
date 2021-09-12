c82a698862cb3b3ee286d3fcf6f853cedfb1d26a
viniciusdm <viniciusbuenodemoraes@gmail.com>
2021-09-12 17:15:37 -0300

    oii

--- Aula5/TesteEx2.cpp
+++ Aula5/TesteEx2.cpp
@@ -9,18 +9,21 @@ void teste() {
     Musica *m1 = new Musica(162, "Tiro ao alvaro");
     Musica *m2 = new Musica(162, "Triste");
     Musica *m3 = new Musica(214, "Aguas de marco");
-    Musica *m3 = new Musica(228, "Bala com bala");
+    Musica *m4 = new Musica(228, "Bala com bala");
 
-    Artista *Elis = new Artista(23, "Elis Regina")ç
+    Artista *Elis = new Artista(23, "Elis Regina");
     Elis -> adicionarMusica(m1);
     Elis -> adicionarMusica(m2);
     Elis -> adicionarMusica(m3);
     Elis -> adicionarMusica(m4);
-    
-    delete Elis;
+
+    cout << "Rodou" << endl;
+
 }
 
 int main() {
+    cout << endl;
     teste();
+    cout << endl;
     return 0;
 }
